bbcf4656fb76ef5dac2556a9f7e8f76f88b236b2
Ulises Rey <ulises.rey@univie.ac.at>
2024-01-26 18:42:49 +0100

    added SetUpClass for test

--- movie_project/test/decompress_read_tests.py
+++ movie_project/test/decompress_read_tests.py
@@ -5,10 +5,14 @@ from movie_project.src.decompress_read import read_and_merge_csv
 
 class DecompressAndRead(unittest.TestCase):
     
-    def test_merged_df_length(self):
-        csv_list = glob.glob("movie_project/data/*.csv")
+    @classmethod
+    def setUpClass(cls):
+        print("Finding csv files")
+        cls._csv_list = glob.glob("movie_project/data/*.csv")
         print("list of csv is:", csv_list)
-        self.assertTrue(len(read_and_merge_csv(csv_list, id="id"))==486414)
+
+    def test_merged_df_length(self):
+        self.assertTrue(len(read_and_merge_csv(self._csv_list, id="id"))==486414)
 
 
 unittest.main(argv=['first-arg-is-ignored'], verbosity=2, exit=False)
