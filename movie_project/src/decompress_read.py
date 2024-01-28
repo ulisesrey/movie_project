3a07cf99e52727f636acc4e35fdeb9dbc5eadc90
Ulises Rey <ulises.rey@univie.ac.at>
2024-01-28 22:02:57 +0100

    refractored

--- movie_project/src/decompress_read.py
+++ movie_project/src/decompress_read.py
@@ -5,9 +5,11 @@ TODO: add docstring
 import os
 import zipfile
 import tarfile
-import pandas as pd
 import time
 import csv
+import pandas as pd
+
+
 
 def decompress_file(file_path, output_dir=None):
     """
@@ -17,38 +19,42 @@ def decompress_file(file_path, output_dir=None):
     file_path: str
         The path of the input file (.zip or .tar.gz).
     output_dir: str, optional
-        The directory where the contents should be extracted. If not provided, it defaults to the directory of the input file.
+        The directory where the contents should be extracted.
+        If not provided, it defaults to the directory of the input file.
     """
     file_extension = os.path.splitext(file_path)[1]
-    
+
     # Set the default extraction directory to the directory of the input file
     extract_dir = output_dir or os.path.dirname(file_path)
 
     if file_extension == '.zip':
         with zipfile.ZipFile(file_path, 'r') as zip_ref:
-            zip_ref.extractall(extract_dir)  # Extract in the specified or default directory
+            # Extract in the specified or default directory
+            zip_ref.extractall(extract_dir)
         print(f"File {file_path} has been successfully decompressed in: ", extract_dir)
     elif file_extension == '.tar.gz':
         with tarfile.open(file_path, 'r:gz') as tar_ref:
-            tar_ref.extractall(extract_dir)  # Extract in the specified or default directory
+            # Extract in the specified or default directory
+            tar_ref.extractall(extract_dir)
         print(f"File {file_path} has been successfully decompressed in: ", extract_dir)
     else:
         print(f"Error: File {file_path} is not in zip or tar.gz format.")
 
 
-def read_and_merge_csv_files(csv_files, id="id"):
+def read_and_merge_csv_files(csv_files, my_id):
     """
     Merge multiple CSV files into a single DataFrame based on the 'ID' column.
 
     Parameters:
+    ------------
     - csv_files (list): List of paths to CSV files.
-
+    - id (str): The column name to use as the key for merging the DataFrames.
     Returns:
     - pd.DataFrame: Merged DataFrame.
     """
 
     start_time = time.time()
-    
+
     # Initialize an empty DataFrame
     merged_df = pd.DataFrame()
 
@@ -61,14 +67,14 @@ def read_and_merge_csv_files(csv_files, id="id"):
             merged_df = df
         else:
             # Merge with the existing DataFrame based on the 'ID' column
-            merged_df = pd.merge(merged_df, df, on='id', how="outer")
+            merged_df = pd.merge(merged_df, df, on=my_id, how="outer")
     end_time = time.time()
     print(f"Processing time: {end_time - start_time} seconds")
 
     return merged_df
 
 
-def read_and_merge_csv_to_dict(csv_list, id="id"):
+def read_and_merge_csv_to_dict(csv_list, my_id):
     """
     Read multiple CSV files and merge them into a single dictionary using the specified key.
 
@@ -77,7 +83,8 @@ def read_and_merge_csv_to_dict(csv_list, id="id"):
     csv_list: list
         A list of paths to the CSV files to be read and merged.
     id: str, optional
-        The column name to use as the key for merging the dictionaries. Default is "id".
+        The column name to use as the key for merging the dictionaries.
+        Default is "id".
 
     Returns:
     ------------
@@ -91,13 +98,9 @@ def read_and_merge_csv_to_dict(csv_list, id="id"):
         with open(csv_file, 'r') as f:
             reader = csv.DictReader(f)
             for row in reader:
-                merged_dict[row[id]] = row
+                merged_dict[row[my_id]] = row
 
     end_time = time.time()
     print(f"Processing time: {end_time - start_time} seconds")
 
     return merged_dict
-
-# Example usage
-#decompress_file('/path/to/TMDB.zip')
-
